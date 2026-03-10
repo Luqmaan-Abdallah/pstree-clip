5e9373553a0b55b88b0e63e6c3f0f8a99ba7047d
Luqmaan Abdallah <luqmaana16@gmail.com>
2026-03-10 23:55:58 +0300

    Add .treeignore basic implementation

--- pstree-clip.ps1
+++ pstree-clip.ps1
@@ -1,17 +1,33 @@
 $RootPath = Resolve-Path "."
+$IgnoreFile = Join-Path $RootPath.Path ".treeignore"
+$IgnoreList = @(".treeignore")
+
+if (Test-Path $IgnoreFile) {
+    $IgnoreList += Get-Content $IgnoreFile | Where-Object { $_ -match '\S' }
+}
+
 $report = [System.Text.StringBuilder]::new()
 
-$allFiles = Get-ChildItem -Path $RootPath -Recurse
+$allFiles = Get-ChildItem -Path $RootPath -Recurse | Where-Object {
+    $shouldIgnore = $false
+    foreach ($pattern in $IgnoreList) {
+        if ($_.FullName -match [regex]::Escape($pattern)) { 
+            $shouldIgnore = $true
+            break 
+        }
+    }
+    !$shouldIgnore
+}
 
 foreach ($item in $allFiles) {
-    $RelativePath = $item.FullName.Replace($RootPath.Path, "")
-    $Depth = ($RelativePath -split '\\').Count - 1
+    $RelativePath = $item.FullName.Replace($RootPath.Path, "").TrimStart('\')
+    $Depth = if ($RelativePath -eq "") { 0 } else { ($RelativePath -split '\\').Count - 1 }
+    
     $Indent = "  " * $Depth
-
     $symbol = if ($item.PSIsContainer) { "+ " } else { "- " }
 
     [void]$report.AppendLine("$Indent$symbol$($item.Name)")
 }
 
 $report.ToString() | Set-Clipboard
-Write-Host "Success! The recursive tree is in your clipboard." -ForegroundColor Green
+Write-Host "Success! The tree is in your clipboard." -ForegroundColor Green
